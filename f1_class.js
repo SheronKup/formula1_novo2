907110e5f2f7c646d256c6844e9d4c091a36f6a9
Enzo <enzo_reis@estudante.sesisenai.org.br>
2024-03-04 09:20:35 -0300

    classe carro e obj pronta

---
+++ f1_class.js
@@ -0,0 +1,22 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w 
+        this.h = h 
+        this.a = a 
+    }
+
+}
+
+class Carro extends Obj{
+    dir = 0
+    dri = 0
+    pts = 0
+    life = 5
+
+    draw_carro(){
+    
+    }
+}
+
